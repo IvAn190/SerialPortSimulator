3593483ed1f02099158c88822eb8b06fe1de6466
Ivan Rey PC <ivanreygarcia2001@hotmail.com>
2024-08-12 23:04:43 +0200

    style(bib): remove some comments

--- bib/defines.h
+++ bib/defines.h
@@ -7,7 +7,6 @@ const char* k = "[+]";
 const char* e = "[-]";
 const char* i = "[*]";
 
-// Definición de macros que aceptan múltiples argumentos
 #define DEBUG_OK(...) printf("[+] " __VA_ARGS__)
 #define DEBUG_ERROR(...) printf("[-] " __VA_ARGS__)
 #define DEBUG_INFO(...) printf("[*] " __VA_ARGS__)
