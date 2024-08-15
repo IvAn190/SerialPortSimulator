89001bec3620b0fa243a352b1a887714b5eb077f
Ivan Rey PC <ivanreygarcia2001@hotmail.com>
2024-08-15 20:31:22 +0200

    feat(sender): added a folder for the sender section

---
+++ sender/sender.c
@@ -0,0 +1,65 @@
+#include <windows.h>
+#include <stdlib.h>
+#include "../bib/defines.h"
+
+// TODO:
+// 1. El programa una vez se ejecuta y el receptor deja de recivir datos porque ha puesto un tiempo t = 1, si el sender sigue teniendo datos,
+//    va a seguir intentando enviar dichos datos.
+//         1.1 Solucion: hacer un contador que mire si se estan enviadno datos, sino, se para el programa a los x segundos.
+
+int main(int argc, char *argv[]) {
+    int ret = EXIT_FAILURE;
+    if (argc < 3) {
+        DEBUG_ERROR("Needs 2 args to work! (port name & data file path)");
+        goto end;
+    }
+
+    HANDLE hSerial, hFile;
+
+    char* comPort = argv[1];
+    char serialPort[20];
+    sprintf(serialPort, "\\\\.\\%s", comPort);
+
+    char* filePath = argv[2];
+
+    /* Open serial port */
+    hSerial = CreateFile(serialPort, (GENERIC_READ | GENERIC_WRITE), 0, NULL, OPEN_EXISTING, 0, NULL);
+    if (hSerial == INVALID_HANDLE_VALUE) {
+        DWORD err = GetLastError();
+        DEBUG_ERROR("Error opening serial port %s. Error code: %lu\n", comPort, err);
+        goto end;
+    } else {
+        DEBUG_OK("Serial port %s opened successfully!\n", comPort);
+    }
+
+    /* Open file */
+    hFile = CreateFile(filePath, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);  
+    if (hFile == INVALID_HANDLE_VALUE) {
+        DWORD err = GetLastError();
+        DEBUG_ERROR("Error opening file %s. Error code: %lu\n", filePath, err);
+        goto end;
+    } else {
+        DEBUG_OK("File %s opened successfully!\n", filePath);
+    }
+
+    BYTE buffer[4096];
+    DWORD bytesRead, bytesWritten;
+
+    DEBUG_INFO("Sending data ...\n");
+    while (ReadFile(hFile, buffer, sizeof(buffer), &bytesRead, NULL) && bytesRead > 0) {
+        if (!WriteFile(hSerial, buffer, bytesRead, &bytesWritten, NULL)) {
+            DWORD err = GetLastError();
+            DEBUG_ERROR("Error writing to the serial port. Error code: %lu\n", err);
+            goto end;
+        } 
+        // else {
+        //     DEBUG_OK("%lu bytes written to the serial port\n", bytesWritten);
+        // }
+    }
+    DEBUG_OK("All data has been sent!\n");
+    ret = EXIT_SUCCESS;
+end:
+    if (hFile) CloseHandle(hFile);
+    if (hSerial) CloseHandle(hSerial);
+    return ret;
+}
